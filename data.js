5430402798c383516dec56477e748a9d0f1ebdd5
GodugunuruSameera <samerra.0909@gmail.com>
2026-03-01 13:27:12 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+// Product array simulating a database (Now with your custom online images!)
+const products = [
+  { 
+    id: 1, 
+    name: "Assorted Fresh Fruits", 
+    category: "Fruits", 
+    price: 120, 
+    stock: 15, 
+    img: "https://www.thehealthy.com/wp-content/uploads/2025/05/GettyImages-2178111688.jpg" 
+  },
+  { 
+    id: 2, 
+    name: "Farm Fresh Vegetables", 
+    category: "Vegetables", 
+    price: 60, 
+    stock: 20, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ4S_s3CerUq_QDacoVwSfgJA0nd4s1OO4FQ&s" 
+  },
+  { 
+    id: 3, 
+    name: "Premium Mixed Spices", 
+    category: "Spices", 
+    price: 200, 
+    stock: 0, /* Set to 0 to show the "Out of Stock" feature working */
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRej9ZqovanR722AG3peGbq-3hzTevoPDtKWA&s" 
+  },
+  { 
+    id: 4, 
+    name: "Healthy Organic Millets", 
+    category: "Millets", 
+    price: 90, 
+    stock: 10, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM_vyqGpCO_o_RGgcLJRmSAd3qLQilZ3mfaw&s" 
+  }
+];
